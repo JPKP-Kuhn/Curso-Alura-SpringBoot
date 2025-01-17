db5cde7df393b1a56ae542f6da1859101323ac5e
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2025-01-17 15:27:01 -0300

    Utilizando anotações do spring data jpa para cadastrar medicos no mysql

--- api/src/main/java/med/vol/api/endereco/DadosEndereco.java
+++ api/src/main/java/med/vol/api/endereco/DadosEndereco.java
@@ -1,10 +1,21 @@
 package med.vol.api.endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+import java.beans.Transient;
+
 public record DadosEndereco(
+        @NotBlank
         String logradouro,
+        @NotBlank
         String bairro,
+        @NotBlank
+                @Pattern(regexp = "\\d{8}")
         String cep,
+        @NotBlank
         String cidade,
+        @NotBlank
         String uf,
         String numero,
         String complemento
